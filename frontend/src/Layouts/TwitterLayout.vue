9b613ff66c26cd4e2c9be7110fb059508fe9aae1
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-05 19:10:37 +0300

    init

--- frontend/src/Layouts/TwitterLayout.vue
+++ frontend/src/Layouts/TwitterLayout.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import Twitter from 'vue-material-design-icons/Twitter.vue'
-// import X from 'vue-material-design-icons/x.vue'
 import Magnify from 'vue-material-design-icons/Magnify.vue'
 import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue'
 import Feather from 'vue-material-design-icons/Feather.vue'
@@ -20,7 +19,7 @@ import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue'
         <div class="flex mx-auto max-w-[1400px]">
             <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:mx-auto">
                 <p-2 class="px-3 mb-4">
-                    <Twitter fillColor="#FFFFFF" :size="37"/>
+                    <img src="../assets/xlogo-removebg-preview.png" class="w-[50px]" alt="">
                 </p-2>
             </div>
         </div>

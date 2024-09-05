c2f8fc2d5da9a8c65895ba7153c06c81ee179a50
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-05 19:23:31 +0300

    init

--- frontend/src/Layouts/TwitterLayout.vue
+++ frontend/src/Layouts/TwitterLayout.vue
@@ -12,15 +12,18 @@ import ImageOutline from 'vue-material-design-icons/ImageOutline.vue'
 import FileGiftBox from 'vue-material-design-icons/FileGifBox.vue'
 import Emoticon from 'vue-material-design-icons/Emoticon.vue'
 import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue'
+import MenuItem from '@/components/MenuItem.vue'
 </script>
 
 <template>
     <div class="fixed w-full ">
         <div class="flex mx-auto max-w-[1400px]">
-            <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:mx-auto">
-                <p-2 class="px-3 mb-4">
+            <div class="lg:w-3/12 w-[60px] h-[100vh] lg:px-4 max-w-[350px] lg:mx-auto">
+                <div class="p-2 px-3 mb-4">
                     <img src="../assets/xlogo-removebg-preview.png" class="w-[50px]" alt="">
-                </p-2>
+                </div>
+
+                <MenuItem iconString="home" />
             </div>
         </div>
     </div>
---
+++ frontend/src/components/MenuItem.vue
@@ -0,0 +1,29 @@
+<script>
+    import { toRefs } from 'vue';
+    import Home from 'vue-material-design-icons/Home.vue'
+    import Pound from 'vue-material-design-icons/Pound.vue'
+    import BellOutline from 'vue-material-design-icons/BellOutline.vue'
+    import AccountOutline from 'vue-material-design-icons/AccountOutline.vue'
+    import EmailOutline from 'vue-material-design-icons/EmailOutline.vue'
+
+    const props = defineProps({
+        iconString: String,
+    })
+
+    const {iconString} = toRefs(props)
+
+    let icon = null
+
+    if (iconString.value === 'Home') icon = Home
+    if (iconString.value === 'Pound') icon = Pound
+    if (iconString.value === 'BellOutline') icon = BellOutline
+    if (iconString.value === 'AccountOutline') icon = AccountOutline
+    if (iconString.value === 'EmailOutline') icon = EmailOutline
+
+
+    
+</script>
+
+<template>
+    
+</template>

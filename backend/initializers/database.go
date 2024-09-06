d947e682bfda6903c895be7d1a775fed6d63cdee
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-06 17:31:17 +0300

    adds migration

--- backend/initializers/database.go
+++ backend/initializers/database.go
@@ -1,6 +1,10 @@
 package initializers
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,8 +12,20 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
-	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var err error
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbNme := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbNme)
+	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Printf("Failed to connect to database %v", err)
+	}
 }
 
 func MigrateDB() {

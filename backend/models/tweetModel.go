788730993a5b7a1004c49f1d286db30c3d596ec7
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-08 14:33:22 +0300

    tests tweet controller

--- backend/models/tweetModel.go
+++ backend/models/tweetModel.go
@@ -8,17 +8,14 @@ import (
 
 type Tweet struct {
 	gorm.Model
-	Id     string `gorm:"primaryKey"`
-	Name   string
-	Handle string
-	// Image     string
-	Tweet string
-	// File      string
-	// Video     string
-	Comments  string
-	Retweets  string
-	Likes     string
-	Analytics string
+	Id        string `gorm:"primaryKey" json:"id"`
+	Name      string `json:"name"`
+	Handle    string `json:"handle"`
+	Tweet     string `json:"tweet"`
+	Comments  string `json:"comments"`
+	Retweets  string `json:"retweets"`
+	Likes     string `json:"likes"`
+	Analytics string `json:"analytics"`
 	CreatedAt time.Time
 	DeletedAt gorm.DeletedAt
 }

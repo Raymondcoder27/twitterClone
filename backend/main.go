c391e535a1e07688f29edc05a3e2d90dad067419
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-09 18:57:29 +0300

    fixes more tweet controller bugs

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example.com/twitterClone/controllers"
 	"example.com/twitterClone/initializers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,15 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	config := cors.Config{
+		AllowsOrigins:    []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+	r.Use(cors.New(config))
 	r.GET("/tweets", controllers.GetTweets)
 	r.POST("/createTweet", controllers.CreateTweet)
 	r.DELETE("/tweet/:id", controllers.DeleteTweet)

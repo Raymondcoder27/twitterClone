30b4d7a7d392a1f57545473a5423b929cfded726
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-09 19:15:14 +0300

    init axios on frontend

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,3 +3,19 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: import.meta.env.VITE_APP_BASE_URL
 })
+
+api.interceptors.request.use((config) =>{
+    return config
+})
+
+api.interceptors.response.use(
+    (reponse)=>response,
+    (error) => {
+        if (error.response != undefined && error.response.status === 401){
+
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default api

f9ea174297e5352db35ad08067cc42b12c10d4a0
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-09 19:48:43 +0300

    improves tweetModal structure

--- backend/models/tweetModel.go
+++ backend/models/tweetModel.go
@@ -9,14 +9,17 @@ import (
 
 type Tweet struct {
 	gorm.Model
-	Name      string `json:"name"`
-	Handle    string `json:"handle"`
-	Tweet     string `json:"tweet"`
-	Comments  string `json:"comments"`
-	Retweets  string `json:"retweets"`
-	Likes     string `json:"likes"`
-	Analytics string `json:"analytics"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Name      string    `json:"name"`
+	Handle    string    `json:"handle"`
+	Image     string    `json:"image"` // New field for image
+	Tweet     string    `json:"tweet"`
+	File      string    `json:"file"`     // New field for file
+	IsVideo   bool      `json:"is_video"` // New field for is_video
+	Comments  string    `json:"comments"`
+	Retweets  string    `json:"retweets"`
+	Likes     string    `json:"likes"`
+	Analytics string    `json:"analytics"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt gorm.DeletedAt
 }

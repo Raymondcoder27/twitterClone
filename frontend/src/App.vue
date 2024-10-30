f509b60247e6ad50e14f24dfb12ef89b598a89c1
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-30 22:02:52 +0300

    fixes frontend ui bugs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,46 +1,43 @@
 <script setup>
-import {onMounted, ref} from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import TwitterLayout from './Layouts/TwitterLayout.vue';
-import Tweet from './components/Tweet.vue';
-import api from '@/config/api'
+import { onMounted, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import TwitterLayout from "./Layouts/TwitterLayout.vue";
+import Tweet from "./components/Tweet.vue";
+import api from "@/config/api";
 // import HelloWorld from './components/HelloWorld.vue'
-defineProps({tweets: Array})
+defineProps({ tweets: Array });
 
+const tweets = ref([]);
 
-const tweets = ref([])
-
-
-
-const fetchTweets = async()=> {
-  try{
-    const response = await api.get('/tweets')
-    tweets.value = response.data
+const fetchTweets = async () => {
+  try {
+    const response = await api.get("/tweets");
+    tweets.value = response.data;
     // alert(JSON.stringify(response))
-  }catch(error){
-    console.error('Error fetching tweets', error)
+  } catch (error) {
+    console.error("Error fetching tweets", error);
   }
-}
+};
 
 const handleTweetDeleted = () => {
-  fetchTweets()  // Re-fetch tweets when a tweet is deleted
-}
+  fetchTweets(); // Re-fetch tweets when a tweet is deleted
+};
 
 const handleTweetAdded = () => {
-  fetchTweets()
-}
+  fetchTweets();
+};
 
-onMounted(()=> {
-  fetchTweets()
-})
+onMounted(() => {
+  fetchTweets();
+});
 </script>
 
 <template>
   <TwitterLayout @tweetAdded="handleTweetAdded">
     <div class="text-white">
       <div class="border-b border-b-gray-800 mt-2"></div>
-     <div class="flex" v-for="tweet in tweets" :key="tweet">
-      <!-- <Tweet :tweet="{
+      <div class="flex" v-for="tweet in tweets" :key="tweet">
+        <!-- <Tweet :tweet="{
         name: 'Ronald Mpagi',
         handle: '@Mpagi',
         image: 'https://randomuser.me/api/portraits/men/40.jpg',
@@ -52,9 +49,8 @@ onMounted(()=> {
         likes: '87',
         analytics: '81',
       }" /> -->
-      <Tweet :tweet="tweet" @tweetDeleted="handleTweetDeleted"/>
-     </div>
+        <Tweet :tweet="tweet" @tweetDeleted="handleTweetDeleted" />
+      </div>
     </div>
   </TwitterLayout>
-
 </template>
